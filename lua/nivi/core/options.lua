d25ce5bf26e19cda84c6bc1d9e73dbb85c7e841a
nivicius <niviciusdev@gmail.com>
2024-09-06 02:23:53 -0300

    updated options.lua

--- lua/nivi/core/options.lua
+++ lua/nivi/core/options.lua
@@ -1,26 +1,38 @@
--- change command explorer mode
 vim.cmd("let g:netrw_liststyle = 3")
 
--- variable to short call
 local opt = vim.opt
 
--- set relative number
 opt.relativenumber = true
+opt.number = true
 
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- when mixed case assume case sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 opt.cursorline = true
+
+-- turn on termguicm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorscheme that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text does't shift
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start"
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
-opt.clipboard:append("unnamedplus") -- use system clipboard
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true
-opt.splitbelow = true
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
 
+-- turn off swapfile
+opt.swapfile = false
