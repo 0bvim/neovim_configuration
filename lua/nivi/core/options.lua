bc03d5c61a61b9a8af1eca8829fc2e15f25a475e
nivicius <niviciusdev@gmail.com>
2024-09-06 00:45:28 -0300

    feat: search case, cursor line, background and term colors

--- lua/nivi/core/options.lua
+++ lua/nivi/core/options.lua
@@ -6,3 +6,12 @@ local opt = vim.opt
 
 -- set relative number
 opt.relativenumber = true
+
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- when mixed case assume case sensitive
+
+opt.cursorline = true
+
+opt.termguicolors = true
+
+opt.background = "dark" -- colorscheme that can be light or dark will be made dark
