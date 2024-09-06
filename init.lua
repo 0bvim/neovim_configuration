d4345141fab06201287b3c88a86097bfbf1f545f
nivicius <niviciusdev@gmail.com>
2024-09-06 00:35:04 -0300

    started incremention directories structure and inserting options

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("nivi.core.options")
---
+++ lua/nivi/core/options.lua
@@ -0,0 +1,8 @@
+-- change command explorer mode
+vim.cmd("let g:netrw_liststyle = 3")
+
+-- variable to short call
+local opt = vim.opt
+
+-- set relative number
+opt.relativenumber = true
