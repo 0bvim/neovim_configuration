7c071335f3daef1a78bbef765acd01c474d7fa85
nivicius <niviciusdev@gmail.com>
2024-09-09 09:54:35 -0300

    feat: mini icons and modified which keys

---
+++ lua/nivi/plugins/mini-icons.lua
@@ -0,0 +1,13 @@
+return {
+	"echasnovski/mini.icons",
+	opts = {},
+	lazy = true,
+	specs = {
+		{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+	},
+	init = function()
+		package.preload["nvim-web-devicons"] = function()
+			require("mini.icons").mock_nvim_web_devicons()
+		end
+	end,
+}
