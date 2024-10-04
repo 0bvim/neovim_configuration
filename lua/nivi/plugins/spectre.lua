03be50c7048fe2bd4c301729d3f11aabebbab2ae
vinicius <niviciusdev@gmail.com>
2024-10-03 23:03:32 -0300

    feat: implemented spectre plugin

---
+++ lua/nivi/plugins/spectre.lua
@@ -0,0 +1,30 @@
+return {
+	"nvim-pack/nvim-spectre",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	setup = function()
+		require("spectre").setup({
+			-- Your configuration options here
+			-- For example:
+			color_devicons = true,
+			open_cmd = "vnew",
+			live_update = false,
+			lnum_for_results = true,
+			-- result_padding = '¦  ',
+			-- line_sep_start = '┌-----------------------------------------',
+			-- line_sep       = '└-----------------------------------------',
+			-- highlight = {
+			--     ui = "String",
+			--     search = "DiffChange",
+			--     replace = "DiffDelete"
+			-- },
+			-- mapping={
+			--   ['tab'] = {
+			--       map = '<Tab>',
+			--       cmd = "<cmd>lua require('spectre').tab()<cr>",
+			--       desc = 'next query'
+			--   },
+			--   -- ... other mappings ...
+			-- }
+		})
+	end,
+}
